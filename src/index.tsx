28ca6fdda533d10eaa3d5e6ff2693b4daeea233c
Arichy <452609128@qq.com>
2022-10-01 11:44:47 +0800

    init

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { IObj } from './type';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+let obj: IObj = {
+  age: 13,
+  name: '123',
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const _name = obj.name;
---
+++ src/type.ts
@@ -0,0 +1,4 @@
+export type IObj = {
+  name: string;
+  age: number;
+};
